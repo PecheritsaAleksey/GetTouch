254a661e49d38ffa2b0174ea5add09d626e28247
PecheritsaAleksey <fromzeya@gmail.com>
2022-11-29 19:39:59 +0700

    Fix unsplash crash

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,7 +15,7 @@ function IndexPage() {
       <style>
         {`
             body {
-              background-image: url(https://source.unsplash.com/xrVDYZRGdw4);
+              background-image: url(https://images.unsplash.com/photo-1484417894907-623942c8ee29?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1932&q=80);
             }
         `}
       </style>
